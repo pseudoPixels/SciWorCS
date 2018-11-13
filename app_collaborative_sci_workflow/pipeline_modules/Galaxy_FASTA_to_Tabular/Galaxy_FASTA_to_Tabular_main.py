e3dccca0da149667688fa2b8c759abe1219a2918
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-11-13 13:46:23 -0600

    Fasta to Tabular done

--- app_collaborative_sci_workflow/pipeline_modules/Galaxy_FASTA_to_Tabular/Galaxy_FASTA_to_Tabular_main.py
+++ app_collaborative_sci_workflow/pipeline_modules/Galaxy_FASTA_to_Tabular/Galaxy_FASTA_to_Tabular_main.py
@@ -2,8 +2,7 @@
 import subprocess
 
 pipe = subprocess.Popen(
-    ["/bin/bash", "/home/ubuntu/Webpage/app_collaborative_sci_workflow/External_Libraries/NiCad-4.0/scripts/Rename",
-     granularity, language,
-     potential_clones, renaming,
-     renamed_potential_clones]).communicate()
+    ["python", "/home/ubuntu/Webpage/app_collaborative_sci_workflow/GalaxyToolBase/Imported/fasta_to_tabular.py",
+     fasta_input, tabular_output,
+     keep_first, descr_columns]).communicate()
 
