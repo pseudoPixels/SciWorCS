e518592775b1734f27d247549c16b112310488dd
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-11-27 21:00:58 -0600

    Source diff and diff similarity value added

---
+++ app_collaborative_sci_workflow/pipeline_modules/Source_diff_similarity/Source_diff_similarity_settings.py
@@ -0,0 +1,7 @@
+
+
+granularity = 'functions'
+language = 'java'
+input_source='/home/ubuntu/Webpage/app_collaborative_sci_workflow/workflow_outputs/test_workflow/filter.xml'
+abstraction='none'
+output_destination= '/home/ubuntu/Webpage/app_collaborative_sci_workflow/workflow_outputs/test_workflow/abstract'
